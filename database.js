7a0870f97740cea1d23e33e67087d93be0acb430
matheusbm8510 <matheusbatistamedeiros9@gmail.com>
2025-11-11 18:08:53 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,100 @@
+const jogadores = [
+    {
+        posicao: "goleiro", 
+
+        jogadores: [
+            {
+                nome: "Alisson",
+                overall: 90,
+                time: "",
+                time-img: "",
+                liga: "",
+                liga-img:
+                pais: "",
+                foto: "",
+            }
+        ]
+
+
+    }
+    {
+        posicao: "zagueiro", 
+
+        jogadores: [
+            {
+                nome: "",
+                overall: ~,
+                time: "",
+                liga: "",
+                pais: "",
+                foto: "",
+            }
+        ]
+
+
+    }
+    {
+        posicao: "lateral", 
+
+        jogadores: [
+            {
+                nome: "",
+                overall: ~,
+                time: "",
+                liga: "",
+                pais: "",
+                foto: "",
+            }
+        ]
+
+
+    }
+    {
+        posicao: "meiocampo", 
+
+        jogadores: [
+            {
+                nome: "",
+                overall: ~,
+                time: "",
+                liga: "",
+                pais: "",
+                foto: "",
+            }
+        ]
+
+
+    }
+    {
+        posicao: "ponta", 
+
+        jogadores: [
+            {
+                nome: "",
+                overall: ~,
+                time: "",
+                liga: "",
+                pais: "",
+                foto: "",
+            }
+        ]
+
+
+    }
+    {
+        posicao: "atacante", 
+
+        jogadores: [
+            {
+                nome: "",
+                overall: ~,
+                time: "",
+                liga: "",
+                pais: "",
+                foto: "",
+            }
+        ]
+
+
+    }
+]
