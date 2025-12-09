cd345403115ef04c39d2a4549a2a1a40d3b96560
matheusbm8510 <matheusbatistamedeiros9@gmail.com>
2025-12-09 15:22:40 -0300

    Add files via upload

--- scripts/cartas.js
+++ scripts/cartas.js
@@ -4,7 +4,7 @@ const zagueiroA = document.querySelector("#zga");
 const zagueiroB = document.querySelector("#zgb");
 const lateralA = document.querySelector("#laa");
 const lateralB = document.querySelector("#lab");
-const volante = document.querySelector("#cmd");
+const volante = document.querySelector("#cdm");
 const meioA= document.querySelector("#cma");
 const meioB = document.querySelector("#cmb");
 const pontaA = document.querySelector("#pe");
@@ -12,22 +12,35 @@ const pontaB = document.querySelector("#pd");
 const atacante = document.querySelector("#at");
 // Cartas Individuais ^
 const cartasEl = document.querySelectorAll(".carta");
+let cartaSelecionadaCampo = null;
+let posicaoElGlobal = null;
+
+overaisCartas = document.querySelector(".carta .overall");
 
 
 const telaSelecaoEl = document.querySelector("#escolher-carta");
 
+let overallTimeEl = 0;
+
+
 for(let cartaAtual of cartasEl) {
     cartaAtual.addEventListener("click", function(e) {
-            
+        
+        if (telaSelecaoEl.classList.contains("tela-selecionada")) {
+            return;
+        }
+        
         const estaSelecionado = cartaAtual.classList.contains("selecionada");
         if (estaSelecionado) {
             return;
         }
+        cartaSelecionadaCampo = cartaAtual;
         telaSelecaoEl.classList.remove('tela-nao-selecionada');        
         telaSelecaoEl.classList.add('tela-selecionada');
+        telaSelecaoEl.style.display = "flex";
 
         const posicaoCarta = cartaAtual.querySelector('.posicao').textContent;
-        criaCartas(posicao);
+        criaCartas(posicaoCarta);
 
 
 
@@ -36,6 +49,7 @@ for(let cartaAtual of cartasEl) {
 
 function criaCartas(posicao) {
     let posicaoEl;
+
     switch (posicao) {
         case "gk":
             posicaoEl = 0;
@@ -43,7 +57,10 @@ function criaCartas(posicao) {
         case "zg":
             posicaoEl = 1;
             break;
-        case "lat":
+        case "le":
+            posicaoEl = 2;
+            break;
+        case "ld":
             posicaoEl = 2;
             break;
         case "cdm":
@@ -56,13 +73,15 @@ function criaCartas(posicao) {
             posicaoEl = 5;
             break;
         case "pd":
-            posicaoEl = 6;
+            posicaoEl = 5;
             break;
-        case "ata":
-            posicaoEl = 7;
+        case "at":
+            posicaoEl = 6;
             break;
     }
     
+    posicaoElGlobal = posicaoEl;
+
     
     const cartasEscolhiveis = document.querySelectorAll(".cartas-escolha");
 
@@ -70,26 +89,106 @@ function criaCartas(posicao) {
     let vetor = [];
     for(let i = 0; vetor.length < 5; i++) {
         let tamanho = jogadores[posicaoEl].jogadoresPosicao.length;
-        const numAleatorio = Math.floor(Math.random() * tamanho) + 1;
-        if(!resultado.includes(numAleatorio)) {
+        const numAleatorio = Math.floor(Math.random() * tamanho);
+        if(!vetor.includes(numAleatorio)) {
             vetor.push(numAleatorio);
         }
     }
     
     for (let i =0 ; i < 5; i++) {
+        const cartaEscolhivelAtual = cartasEscolhiveis[i];
+        cartaEscolhivelAtual.style.display="auto";
+
         let overallEl = cartaEscolhivelAtual.querySelector('.overall');
-        let posicaoEl = cartaEscolhivelAtual.querySelector('.posicao');
+        let posicaoCartaEl = cartaEscolhivelAtual.querySelector('.posicao');
         let bandeiraEl = cartaEscolhivelAtual.querySelector('.bandeira');
         let ligaEl = cartaEscolhivelAtual.querySelector('.liga');
         let fotoEl = cartaEscolhivelAtual.querySelector('.foto');
         let timeEl = cartaEscolhivelAtual.querySelector('.time');
         let nomeEl = cartaEscolhivelAtual.querySelector('.nome');
         // ^ cartas atuais 
+
+        let j = jogadores[posicaoEl].jogadoresPosicao[vetor[i]];
+
+
         overallEl.innerHTML = jogadores[posicaoEl].jogadoresPosicao[vetor[i]].overall;
-        posicaoEl.innerHTML = posicao;
+        posicaoCartaEl.textContent = posicao;
         bandeiraEl.src = "https://flagsapi.com/"+jogadores[posicaoEl].jogadoresPosicao[vetor[i]].pais+"/flat/64.png"
-        timeEl.innerHTML = jogadores[posicaoEl].jogadoresPosicao[vetor[i]].time;
+
+        const bandeiraAtual = jogadores[posicaoEl].jogadoresPosicao[vetor[i]].pais;
+        if (bandeiraAtual === "GB") {
+            let membroUK = jogadores[posicaoEl].jogadoresPosicao[vetor[i]].uk;
+            bandeiraEl.src = "imgs/"+membroUK+".svg"
+        }
+
+        timeEl.src = jogadores[posicaoEl].jogadoresPosicao[vetor[i]].timeimg;
         fotoEl.src = jogadores[posicaoEl].jogadoresPosicao[vetor[i]].foto;
-        cartaEscolhivelAtual.style.backgroundImage = url("imgs/"))
+        ligaEl.src = "imgs/"+jogadores[posicaoEl].jogadoresPosicao[vetor[i]].liga+".png";
+        nomeEl.innerHTML = jogadores[posicaoEl].jogadoresPosicao[vetor[i]].nome;
+        cartaEscolhivelAtual.style.display = "block";
+        cartaEscolhivelAtual.style.backgroundImage = 'url("imgs/carta.png")';
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+        // transicaozinha daorinha :D
+
+        cartaEscolhivelAtual.addEventListener('click', function(e) {
+            colocaJogadorNoCampo(j);
+            
+        })
+
+
+        
     }
+}
+
+function colocaJogadorNoCampo(jogador) {
+    let lista = jogadores[posicaoElGlobal].jogadoresPosicao;
+
+    let index = lista.findIndex(p => p.nome === jogador.nome);
+
+    if (index !== -1) {
+        lista.splice(index, 1);
+    }
+    
+    
+    
+    if (!cartaSelecionadaCampo) return;
+    cartaSelecionadaCampo.querySelector('.overall').textContent = jogador.overall;
+    cartaSelecionadaCampo.querySelector('.liga').src = "imgs/"+jogador.liga+".png";
+    cartaSelecionadaCampo.querySelector('.foto').src = jogador.foto;
+    cartaSelecionadaCampo.querySelector('.bandeira').src = "https://flagsapi.com/"+jogador.pais+"/flat/64.png";
+    const bandeiraEl = cartaSelecionadaCampo.querySelector('.bandeira');
+    bandeiraEl.src = "https://flagsapi.com/" + jogador.pais + "/flat/64.png";
+
+    const bandeiraAtual = jogador.pais;
+
+    if (bandeiraAtual === "GB") {
+        let membroUK = jogador.uk;
+        bandeiraEl.src = "imgs/" + membroUK + ".svg";
+    }
+
+    cartaSelecionadaCampo.querySelector('.nome').textContent = jogador.nome;
+    cartaSelecionadaCampo.querySelector('.time').src = jogador.timeimg
+
+
+
+    telaSelecaoEl.classList.remove('tela-selecionada');
+    telaSelecaoEl.classList.add('tela-nao-selecionada');
+    cartaSelecionadaCampo.classList.add('selecionada');
+    const cartas = document.querySelectorAll(".cartas-escolha");
+    cartas.forEach(c => c.style.display = "none");
+    cartaSelecionadaCampo.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    });
+}
+
+
+const estrelasTd = document.querySelectorAll(".estrelas");
+
+function atualizaEstrela() {
+    
 }
